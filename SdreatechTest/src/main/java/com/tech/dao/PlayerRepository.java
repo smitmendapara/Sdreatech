077def0a257da7e3b94621fd1833bb1c1e097e3d
Smit Mendapara <69449110+smitmendapara@users.noreply.github.com>
2022-06-18 10:19:17 +0530

    Delete SdreatechTest directory

--- SdreatechTest/src/main/java/com/tech/dao/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tech.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tech.modal.Player;
-
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-
-}
--- SdreatechTest/src/main/java/com/tech/dao/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tech.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tech.modal.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-
-}
